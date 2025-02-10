67eeccc396630b7823e22367c4c22b9a11e0df89
Alexander <alexandervolzhanin2004@gmail.com>
2025-02-10 20:30:06 +0300

    Добавил задания

---
+++ Задания/Задание 1.1/grav/cache/compiled/files/3046b40f21e878fe90fb28dc12857db3.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/var/www/html/user/config/security.yaml',
+    'modified' => 1739113261,
+    'data' => [
+        'salt' => '4uv6rLWAmi3dPF'
+    ]
+];
